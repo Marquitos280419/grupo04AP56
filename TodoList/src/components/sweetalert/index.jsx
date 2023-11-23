5709971a468d5f7c2a4f5e108f3060fff879c0fc
Marcos Barey <caposelmarco@gmail.com>
2023-11-23 18:19:19 -0300

    Create SweetAlert

---
+++ TodoList/src/components/sweetalert/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import Swal from "sweetalert2";
+function SweetAlert({ message, type}) {
+  Swal.fire({
+    text: message,
+    icon: type,
+  });
+
+  return null; 
+}
+SweetAlert.propTypes = {
+    message: PropTypes.shape({
+    }).isRequired,
+    type: PropTypes.shape({
+    }).isRequired,
+  
+  };
+export default SweetAlert;
