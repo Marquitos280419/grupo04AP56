a9c135a1c302e44cbdf1ff8c96f731f3daef87c4
Marcos Barey <caposelmarco@gmail.com>
2023-11-23 18:21:52 -0300

    Update

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
+import Grid from "@mui/material/Grid";
+import TaskList from "./components/tasklist";
 
-import TaskForm from "./components/taskform";
+import {
+  Card,
+  Typography,
+ 
+  CardContent,
+ 
+} from "@mui/material";
 
 function App() {
   return (
     <>
-      <TaskForm />
+      <Card sx={{ minWidth: 500 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom >
+            Tareas
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TaskList/>
+            </Grid>
+            <Grid item xs={12}>
+            </Grid>
+            <Grid item xs={4}></Grid>
+            <Grid item xs={8}></Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </>
   );
 }
